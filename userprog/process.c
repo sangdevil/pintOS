27040e46131b7180f34528576bcb68a2fc83477a
castlecowrice <swh98@kaist.ac.kr>
2023-04-05 09:01:30 +0000

    exit, exec, fork, wait done (for backup)

--- userprog/process.c
+++ userprog/process.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "userprog/syscall.h"
+#include "lib/user/syscall.h"
 #include "userprog/gdt.h"
 #include "userprog/tss.h"
 #include "filesys/directory.h"
@@ -17,10 +19,13 @@
 #include "threads/thread.h"
 #include "threads/mmu.h"
 #include "threads/vaddr.h"
+#include "threads/synch.h"
 #include "intrinsic.h"
 #ifdef VM
 #include "vm/vm.h"
 #endif
+
+/* 주의: process_fork, duplicate_pte, __do_fork 함수 테스트 안 됐음. */
 
 #define MAX_ARGC 128
 //max. number of command line arguments
@@ -29,6 +34,15 @@ static void process_cleanup (void);
 static bool load (const char *file_name, struct intr_frame *if_);
 static void initd (void *f_name);
 static void __do_fork (void *);
+
+struct waiter {
+	struct semaphore *sema_wait;	/* for synchronization */
+	struct lock *lock;				/* for accessing exited and exit_code */
+	bool *exited;					/* has the parent(or child) already exited? */
+	int *exit_code;					/* exit code of parent(or child) */
+	tid_t tid;						/* tid of parent(or child) */
+	struct list_elem elem;
+};
 
 /* General process initializer for initd and other process. */
 static void
@@ -49,12 +63,13 @@ process_create_initd (const char *file_name) {
 	/* Make a copy of FILE_NAME.
 	 * Otherwise there's a race between the caller and load(). */
 	fn_copy = palloc_get_page (0);
+	//msg("get page again = %p", palloc_get_page(0));
 	if (fn_copy == NULL)
 		return TID_ERROR;
 	strlcpy (fn_copy, file_name, PGSIZE);
 
 	char *saveptr;
-	file_name = strtok_r(file_name, " ", &saveptr); // truncates any arguments following actual file name.
+	file_name = strtok_r(file_name, " ", &saveptr); // truncates any arguments following actual file name. (?)
 	/* Create a new thread to execute FILE_NAME. */
 	tid = thread_create (file_name, PRI_DEFAULT, initd, fn_copy);
 	if (tid == TID_ERROR)
@@ -81,8 +96,54 @@ initd (void *f_name) {
 tid_t
 process_fork (const char *name, struct intr_frame *if_ UNUSED) {
 	/* Clone current thread to new thread.*/
-	return thread_create (name,
-			PRI_DEFAULT, __do_fork, thread_current ());
+	struct semaphore sema_fork; // synchronization: __do_fork done -> process_fork done
+	sema_init(&sema_fork, 0); 
+
+	/* for process_wait() */
+	struct waiter *w = (struct waiter *)malloc(sizeof (struct waiter));
+	struct semaphore *sema_wait = (struct semaphore *)malloc(sizeof (struct semaphore));
+	struct lock *lock = (struct lock *)malloc(sizeof (struct lock));
+	bool *exited = (bool *)malloc(sizeof (bool));
+	int *exit_code = (int *)malloc(sizeof (int));
+
+	sema_init(sema_wait, 0);
+	lock_init(lock);
+	*exited = false;
+
+	w->sema_wait = sema_wait;
+	w->lock = lock;
+	w->exited = exited;
+	w->exit_code = exit_code;
+	//printf("Hi\n");
+	struct thread *cur = thread_current();
+	void *args[] = {cur, if_, &sema_fork, w};
+	tid_t tid = thread_create (name,
+			PRI_DEFAULT, __do_fork, (void *)args);
+			// PRI_DEFAULT, __do_fork, thread_current ());
+	//printf("Hi\n");
+	if(tid == TID_ERROR) {
+		//printf("Hi\n");
+		goto error;
+	}
+	//printf("Hi\n");
+	sema_down(&sema_fork);
+	if(cur->fork_error) {
+		//printf("Hi\n");
+		goto error;
+	}
+
+	//printf("Hi\n");
+	w->tid = tid;
+	list_push_back(&cur->down_list, &w->elem);
+	return tid;
+
+error:
+	free(w);
+	free(sema_wait);
+	free(lock);
+	free(exited);
+	free(exit_code);
+	return TID_ERROR;
 }
 
 #ifndef VM
@@ -97,21 +158,32 @@ duplicate_pte (uint64_t *pte, void *va, void *aux) {
 	bool writable;
 
 	/* 1. TODO: If the parent_page is kernel page, then return immediately. */
+	/*
+	if (parent->pml4 == NULL) {
+		return true;
+	}
+	*/
+	if(is_kernel_vaddr(va)) {
+		return true;
+	}
 
 	/* 2. Resolve VA from the parent's page map level 4. */
 	parent_page = pml4_get_page (parent->pml4, va);
 
 	/* 3. TODO: Allocate new PAL_USER page for the child and set result to
 	 *    TODO: NEWPAGE. */
+	newpage = palloc_get_page(PAL_USER); // ??
 
 	/* 4. TODO: Duplicate parent's page to the new page and
 	 *    TODO: check whether parent's page is writable or not (set WRITABLE
 	 *    TODO: according to the result). */
-
+	memcpy(newpage, parent_page, PGSIZE);
+	writable = (*pte) & PTE_W;
 	/* 5. Add new page to child's page table at address VA with WRITABLE
 	 *    permission. */
 	if (!pml4_set_page (current->pml4, va, newpage, writable)) {
 		/* 6. TODO: if fail to insert page, do error handling. */
+		return false;
 	}
 	return true;
 }
@@ -124,10 +196,13 @@ duplicate_pte (uint64_t *pte, void *va, void *aux) {
 static void
 __do_fork (void *aux) {
 	struct intr_frame if_;
-	struct thread *parent = (struct thread *) aux;
+	// struct thread *parent = (struct thread *) aux;
+	void **args = (void **) aux;
+	struct thread *parent = (struct thread *) args[0];
 	struct thread *current = thread_current ();
 	/* TODO: somehow pass the parent_if. (i.e. process_fork()'s if_) */
-	struct intr_frame *parent_if;
+	struct intr_frame *parent_if = (struct intr_frame *) args[1]; // &parent->tf;
+	struct semaphore *sema_fork = (struct semaphore *) args[2];
 	bool succ = true;
 
 	/* 1. Read the cpu context to local stack. */
@@ -135,6 +210,7 @@ __do_fork (void *aux) {
 
 	/* 2. Duplicate PT */
 	current->pml4 = pml4_create();
+	
 	if (current->pml4 == NULL)
 		goto error;
 
@@ -144,8 +220,10 @@ __do_fork (void *aux) {
 	if (!supplemental_page_table_copy (&current->spt, &parent->spt))
 		goto error;
 #else
-	if (!pml4_for_each (parent->pml4, duplicate_pte, parent))
+	if (!pml4_for_each (parent->pml4, duplicate_pte, parent)) {
+		//printf("Hi\n");
 		goto error;
+	}
 #endif
 
 	/* TODO: Your code goes here.
@@ -153,14 +231,48 @@ __do_fork (void *aux) {
 	 * TODO:       in include/filesys/file.h. Note that parent should not return
 	 * TODO:       from the fork() until this function successfully duplicates
 	 * TODO:       the resources of parent.*/
+	//should change
+	/*
+	struct list *fd_list = &parent->fd_list;
+	for(struct list_elem *e = list_begin(fd_list); e != list_end(fd_list); e = list_next(e)) { 
+		struct file_descriptor *parent_fd = list_entry(e, struct file_descriptor, elem); 
+		struct file *file_copy;
+		if( !(file_copy = file_duplicate(parent_fd->file)) ) {
+			goto error;
+		} 
+		
+		struct file_descriptor *curr_fd = (struct file_descriptor *)malloc(sizeof (struct file_descriptor));
+		curr_fd->fd = parent_fd->fd;
+		curr_fd->file = file_copy;
+		curr_fd->file_size = parent_fd->file_size;
+
+		list_push_back(&current->fd_list, &curr_fd->elem);
+		//list_push_back(&current->fd_list, file_copy);
+	}
+	*/
+
+	/* for process_wait() */
+	struct waiter *w = (struct waiter *) args[3];
+	struct waiter *w_child = (struct waiter *) malloc(sizeof(struct waiter)); //__do_fork never fails from this point?
+	memcpy(w_child, w, sizeof (struct waiter));
+	w_child->tid = parent->tid;
+	list_push_back(&current->up_list, &w_child->elem);
+
+	if_.R.rax = 0; // return value for child = 0.
 
 	process_init ();
 
 	/* Finally, switch to the newly created process. */
-	if (succ)
+	if (succ) {
+		parent->fork_error = false;
+		sema_up(sema_fork);
 		do_iret (&if_);
+	}
 error:
-	thread_exit ();
+	//printf("Hi\n");
+	parent->fork_error = true;
+	sema_up(sema_fork);
+	thread_exit (); //exit code meaningless in this case?
 }
 
 /* Switch the current execution context to the f_name.
@@ -186,13 +298,19 @@ process_exec (void *f_name) {
 	//printf("Load result = %d\n", success);
 
 	/* If load failed, quit. */
-	palloc_free_page (file_name);
+	palloc_free_page (file_name); //f_name should come from palloc_get_page? (for initd, this is true, but what if user calls exec?)
 	if (!success)
 		return -1;
 
 	/* Start switched process. */
 	do_iret (&_if);
 	NOT_REACHED ();
+}
+
+int
+process_wait (tid_t child_tid) {
+	for(long long x = 0; x <= 1000000000LL; x++); //try increasing the value if pintos terminates unexpectedly in the middle.
+	return -1;
 }
 
 
@@ -206,13 +324,54 @@ process_exec (void *f_name) {
  * This function will be implemented in problem 2-2.  For now, it
  * does nothing. */
 int
-process_wait (tid_t child_tid UNUSED) {
+_process_wait (tid_t child_tid UNUSED) {
 	/* XXX: Hint) The pintos exit if process_wait (initd), we recommend you
 	 * XXX:       to add infinite loop here before
 	 * XXX:       implementing the process_wait. */
-	//while(1);
-	for(int x = 0; x <= 1000000000; x++); //waits for reasonable amount of time.
-	return -1;
+	//for(int x = 0; x <= 500000000; x++); //waits for reasonable amount of time.
+	//return -1;
+
+	struct thread *parent = thread_current(); //as a parent thread..
+	struct list *down_list = &parent->down_list;
+	for(struct list_elem *e = list_begin(down_list); e != list_end(down_list);) {
+		//msg("Hi!");
+		struct waiter *w = list_entry(e, struct waiter, elem);
+		if(w->tid != child_tid) {
+			e = list_next(e);
+			continue;
+		}
+
+		lock_acquire(w->lock);
+		if(!(*w->exited)) { //child has not exited 
+			lock_release(w->lock);
+			sema_down(w->sema_wait); //wait for child to sema_up (i.e. exit)
+			lock_acquire(w->lock);
+			ASSERT(*w->exited);
+		}
+		int exit_code = *w->exit_code;
+		lock_release(w->lock);
+
+		//msg("1");
+		free(w->sema_wait);
+		free(w->lock);
+		free(w->exited);
+		free(w->exit_code);
+		//msg("1");
+
+		e = list_remove(e);
+		free(w);
+
+		return exit_code;
+	}
+	return -1; //no child with child_tid.
+
+}
+
+//helper for thread_exit.
+void
+_thread_exit (int status) {
+	thread_current()->exit_code = status;
+	thread_exit();
 }
 
 /* Exit the process. This function is called by thread_exit (). */
@@ -223,7 +382,83 @@ process_exit (void) {
 	 * TODO: Implement process termination message (see
 	 * TODO: project2/process_termination.html).
 	 * TODO: We recommend you to implement process resource cleanup here. */
-	printf ("%s: exit(%d)\n", curr->name, curr->tf.R.rax);
+	printf ("%s: exit(%d)\n", curr->name, curr->exit_code); 
+
+	//close all open files.
+	//free some dynamically-allocated memory.
+	/*
+	struct list *fd_list = &curr->fd_list;
+	for(struct list_elem *e = list_begin(fd_list); e != list_end(fd_list); e = list_remove(e)) {
+		struct file_descriptor *fd = list_entry(e, struct file_descriptor, elem);
+		file_close(fd->file);
+		free(fd);
+	}
+	*/
+
+	/* for process_wait() */
+
+	struct list *up_list = &curr->up_list;
+	for(struct list_elem *e = list_begin(up_list); e != list_end(up_list);) { //there's only 1 parent for each thread, though.
+		struct waiter *w = list_entry(e, struct waiter, elem);
+		
+		lock_acquire(w->lock);
+		if(*w->exited) { //if the parent has already exited, it's the current thread's responsibility to free.
+			lock_release(w->lock);
+			
+			//msg("2");
+			free(w->sema_wait);
+			free(w->lock);
+			free(w->exited);
+			free(w->exit_code);
+			//msg("2");
+			// msg("1");
+			// free(w);
+			// msg("1");
+		}
+		else { //otherwise, let the parent free.
+			*w->exited = true;
+			*w->exit_code = curr->exit_code;
+			lock_release(w->lock);
+			sema_up(w->sema_wait);
+			// msg("2");
+			// free(w);
+			// msg("2");
+		}
+		e = list_remove(e);
+		free(w);
+	}
+
+	struct list *down_list = &curr->down_list;
+	for(struct list_elem *e = list_begin(down_list); e != list_end(down_list);) {
+		struct waiter *w = list_entry(e, struct waiter, elem);
+		
+		lock_acquire(w->lock);
+		if(*w->exited) { //if the child has already exited, it's the current thread's responsibility to free.
+			lock_release(w->lock);
+			
+			//msg("3");
+			free(w->sema_wait);
+			free(w->lock);
+			free(w->exited);
+			free(w->exit_code);
+			//msg("3");
+			// msg("3");
+			// free(w);
+			// msg("3");
+		}
+		else { //otherwise, let the child free.
+			*w->exited = true;
+			*w->exit_code = curr->exit_code;
+			lock_release(w->lock);
+			sema_up(w->sema_wait);
+			// msg("4");
+			// free(w);
+			// msg("4");
+		}
+		e = list_remove(e);
+		free(w);
+	}
+
 
 	process_cleanup ();
 }
@@ -355,11 +590,13 @@ load (const char *file_name, struct intr_frame *if_) {
 	int argc = i;
 
 	/* Open executable file. */
+	//msg(file_name);
 	file = filesys_open (file_name);
 	if (file == NULL) {
 		printf ("load: %s: open failed\n", file_name);
 		goto done;
 	}
+	//msg("open success");
 
 	/* Read and verify executable header. */
 	if (file_read (file, &ehdr, sizeof ehdr) != sizeof ehdr
